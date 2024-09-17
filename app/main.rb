04f45096d6d2c131e98db1875a5ac56597600d46
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-09-17 13:41:32 +0100

    Refactor download_piece command to use OptionParser for better argument handling.

--- app/main.rb
+++ app/main.rb
@@ -43,14 +43,33 @@ when "handshake"
   handshake = peer_connection.perform_handshake!
   puts "Peer ID: #{handshake.peer_id}"
 when "download_piece"
-  metainfo_file = MetainfoFile.parse(File.read(ARGV[1]))
-  peer_ip, peer_port = ARGV[2].split(":")
-  piece_index = ARGV[3].to_i
+  require "optparse"
+
+  options = {}
+
+  OptionParser.new do |opts|
+    opts.banner = "Usage: ./your_bittorrent.sh download_piece [options] <torrent_file> <piece_index>"
+
+    opts.on("-o", "--output FILE", "Output file") do |file|
+      options[:output] = file
+    end
+  end.parse!
+
+  raise OptionParser::MissingArgument, "Output file is required" if options[:output].nil?
+
+  torrent_file_path = ARGV[1]
+  raise OptionParser::MissingArgument, "Torrent file is required" if torrent_file_path.nil?
+
+  piece_index = ARGV[2].to_i
+  raise OptionParser::MissingArgument, "Piece index is required" if piece_index.nil?
+
+  metainfo_file = MetainfoFile.parse(File.read(torrent_file_path))
+  peer_addresses = TrackerClient.new.get_peer_addresses(metainfo_file)
   piece = metainfo_file.pieces[piece_index]
 
   raise "piece index out of bounds (#{piece_index} >= #{metainfo_file.pieces.length})" if piece.nil?
 
-  peer_connection = PeerConnection.new(metainfo_file, PeerAddress.new(peer_ip, peer_port))
+  peer_connection = PeerConnection.new(metainfo_file, peer_addresses.first)
   peer_connection.perform_handshake!
   peer_connection.wait_for_bitfield!
   peer_connection.send_interested!
