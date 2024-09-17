3af94cfa616e1fa4bb1dcf65f9fea0e04d1c9adc
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-09-17 12:04:51 +0100

    Refactor bencode decoding into a BencodeDecoder class with modular methods.

--- app/main.rb
+++ app/main.rb
@@ -1,27 +1,74 @@
 require "json"
+require "stringio"
 
 if ARGV.length < 2
   puts "Usage: your_bittorrent.sh <command> <args>"
-  exit(1)
+  exit 1
 end
 
-def decode_bencode(bencoded_value)
-  if bencoded_value[0].chr.match?(/\d/)
-    first_colon = bencoded_value.index(":")
-    if first_colon.nil?
-      raise ArgumentError, "Invalid encoded value"
+class BencodeDecoder
+  def self.decode(bencoded_value)
+    io = StringIO.new(bencoded_value)
+
+    self.do_decode(io)
+  end
+
+  def self.do_decode(io)
+    char = io.read(1)
+
+    case char
+    when "i"
+      self.decode_integer(io)
+    when "d"
+      self.decode_dictionary(io)
+    when "l"
+      self.decode_list(io)
+    when "1".."9"
+      self.decode_string(io, char)
+    else
+      raise "unexpected character #{char}"
     end
-    bencoded_value[first_colon + 1..-1]
-  else
-    puts "Only strings are supported at the moment"
-    exit(1)
+  end
+
+  def self.decode_integer(io)
+    integer_str = ""
+
+    while
+      char = io.read(1)
+      break if char == "e"
+      integer_str += char
+    end
+
+    integer_str.to_i
+  end
+
+  def self.decode_string(io, first_char)
+    length_str = first_char
+
+    while
+      char = io.read(1)
+      break if char == ":"
+      length_str += char
+    end
+
+    length = length_str.to_i
+    io.read(length)
+  end
+
+  def self.decode_dictionary(io)
+    raise NotImplementedError
+  end
+
+  def self.decode_list(io)
+    raise NotImplementedError
   end
 end
 
 command = ARGV[0]
 
 if command == "decode"
-  encoded_str = ARGV[1]
-  decoded_str = decode_bencode(encoded_str)
-  puts JSON.generate(decoded_str)
+  decoded = BencodeDecoder.decode(ARGV[1])
+  puts JSON.generate(decoded)
+else
+  raise "unsupported command #{command}"
 end
