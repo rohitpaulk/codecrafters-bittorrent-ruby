7bfd59feaaa364cebb23bba2a3c3ca3d2202a503
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-09-17 13:42:50 +0100

    Refactor download_piece command to use output_file_path and improve error handling.

--- app/main.rb
+++ app/main.rb
@@ -45,22 +45,21 @@ when "handshake"
 when "download_piece"
   require "optparse"
 
-  options = {}
+  output_file_path = nil
 
   OptionParser.new do |opts|
     opts.banner = "Usage: ./your_bittorrent.sh download_piece [options] <torrent_file> <piece_index>"
 
     opts.on("-o", "--output FILE", "Output file") do |file|
-      options[:output] = file
+      output_file_path = file
     end
   end.parse!
 
-  raise OptionParser::MissingArgument, "Output file is required" if options[:output].nil?
-
   torrent_file_path = ARGV[1]
-  raise OptionParser::MissingArgument, "Torrent file is required" if torrent_file_path.nil?
-
   piece_index = ARGV[2].to_i
+
+  raise OptionParser::MissingArgument, "Output file is required" if output_file_path.nil?
+  raise OptionParser::MissingArgument, "Torrent file is required" if torrent_file_path.nil?
   raise OptionParser::MissingArgument, "Piece index is required" if piece_index.nil?
 
   metainfo_file = MetainfoFile.parse(File.read(torrent_file_path))
@@ -75,11 +74,14 @@ when "download_piece"
   peer_connection.send_interested!
   peer_connection.wait_for_unchoke!
 
-  piece.blocks.each do |block|
+  block_data_list = piece.blocks.map do |block|
     peer_connection.send_request!(block)
     data = peer_connection.wait_for_piece!(block)
     puts "Downloaded block #{block.index} of piece #{piece.index}"
+    data
   end
+
+  File.write(output_file_path, block_data_list.join)
 else
   raise "unsupported command #{command}"
 end
